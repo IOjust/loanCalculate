38952b816ab1e75382ebef430752bae596c73ec0
月亮 <ax0566@163.com>
2017-03-12 09:40:17 +0800

    初始化

---
+++ loanCalculate/loanCalculate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  loanCalculate
+//
+//  Created by 月亮 on 2017/3/12.
+//  Copyright © 2017年 月亮. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
