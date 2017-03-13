e522ffc833e1e20c580306208fd7daa1774f7737
月亮 <ax0566@163.com>
2017-03-13 18:11:30 +0800

    测试

--- loanCalculate/loanCalculate/AppDelegate.swift
+++ loanCalculate/loanCalculate/AppDelegate.swift
@@ -20,25 +20,45 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        
+        //当应用程序从活动状态到非活动状态，这个发生在突然的打断，比如来电话。或者短信，或者当用户推出应用程序。过渡到后台状态
+        //用这个方法暂停正在进行的任务，禁用计时器 。游戏用这个方法来暂停游戏
+        //        print("applicationWillResignActive 被执行了")
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        //当程序被推送到后台的时候调用。所以要设置后台继续运行，则在这个函数里面设置即可
+        //用这个方法来释放共享的资源 ， 保存用户数据 ，停用计时器。
+        //        print("applicationDidEnterBackground 被执行了")
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        
+        //当程序从后台将要重新回到前台时候调用，你取消进入后台的时候调用的程序
+        //        print("applicationWillEnterForeground 被执行了")
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        //应用程序进入活动状态。这时候需要重新启用应用程序
+        //        print("applicationDidBecomeActive 被执行了")
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        //应用程序将要被终结的时候执行  可以适当保存数据之类的
+        //        print("applicationWillTerminate 被执行了")
+    }
+    
+    func applicationDidReceiveMemoryWarning(_ application: UIApplication) {
+        //如果为应用程序分配了太多内存操作系统会终止应用程序的运行，在终止前会执行这个方法，通常可以在这里进行内存清理工作防止程序被终止
+        //print("applicationDidReceiveMemoryWarning 被执行了")
+    }
+    
+    func applicationSignificantTimeChange(_ application: UIApplication) {
+        //当系统时间发生改变时执行
+    }
+    
+    func application(_ application: UIApplication, willChangeStatusBarFrame newStatusBarFrame: CGRect) {
+        //当StatusBar框将要变化时执行
     }
 
 
--- loanCalculate/loanCalculate/ViewController.swift
+++ loanCalculate/loanCalculate/ViewController.swift
@@ -9,17 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //拉取连接时候，control + click
+    @IBOutlet weak var loanAmountField: UITextField!
+    @IBOutlet weak var loanTermField: UITextField!
+    @IBOutlet weak var interestRateField: UITextField!
+    @IBOutlet weak var result: UILabel!
+    @IBAction func simpleBution(_ sender: Any) {
+        //实例化
+        let calculate1 = simpleCalculate()
+        // !表示强制解析，有可能为nil。
+        result.text = calculate1.calculate(loanAmount: Double( loanAmountField.text!)!, years:Int( loanTermField.text!)!, interestRate: Double( interestRateField.text!)!).description
+        
+    }
+    
+    
+    //MARK:视图加载完成
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    //MARK:接到内存警告
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
 }
+
+
+class simpleCalculate {
+    //变量命令是小驼峰命名法：驼峰式命名，第一个英文单词小写，后面的所有单词首字母大写
+    func calculate(loanAmount:Double,years:Int,interestRate:Double) -> Double {
+        //定义一个常量 let,定义一个变量 var
+        //option + click 查看元素的介绍
+        //command + click 查看源码
+        //struct :结构体，是copy，值传递
+        //class :类，是引用，址传递
+        
+        let rate = interestRate / 100
+        let interest = loanAmount * rate * Double(years)
+        return loanAmount + interest
+    }
+}
+
 
