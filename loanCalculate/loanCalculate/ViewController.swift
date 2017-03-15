48d9c5af44ac5c2e5f19123c5e664eccc98849bf
月亮 <ax0566@163.com>
2017-03-15 16:45:25 +0800

    第二周学习：第一周贷款计算器的简单优化优化

--- loanCalculate/loanCalculate/ViewController.swift
+++ loanCalculate/loanCalculate/ViewController.swift
@@ -14,18 +14,69 @@ class ViewController: UIViewController {
     @IBOutlet weak var loanTermField: UITextField!
     @IBOutlet weak var interestRateField: UITextField!
     @IBOutlet weak var result: UILabel!
+    
     @IBAction func simpleBution(_ sender: Any) {
         //实例化
         let calculate1 = simpleCalculate()
         // !表示强制解析，有可能为nil。
-        result.text = calculate1.calculate(loanAmount: Double( loanAmountField.text!)!, years:Int( loanTermField.text!)!, interestRate: Double( interestRateField.text!)!).description
+        // Optional("") ,去掉optional 直接加 !
+        print("textField的初始值 = \(loanAmountField.text)")
+        // !表示强制解析，nil代表没有值
+        print("textField ！的初始值 = \(loanAmountField.text!)")
+        
+        guard let loanTermField = loanTermField.text,let loanAmountField = loanAmountField.text,
+        let interestRateField = interestRateField.text
+        else {
+            return
+        }
+        guard let loanTerm = Int(loanTermField),let loanAmount = Double(loanAmountField),let interestRate = Double(interestRateField) else {
+            print("用户输入数据格式错误")
+            return
+        }
+        
+        // 看起来特别舒服
+        result.text = calculate1.calculate(loanAmount: loanAmount, years: loanTerm, interestRate: interestRate).money
+        
+        
+//        result.text = calculate1.calculate(loanAmount: Double( loanAmountField.text!)!, years:Int( loanTermField.text!)!, interestRate: Double( interestRateField.text!)!).money
         
     }
     
+    @IBAction func compoundButtion(_ sender: Any) {
+        //复利计算 实例化
+        let calculate2 = compoundCalculate()
+        result.text = calculate2.calculate(loanAmount: Double(loanAmountField.text!)!, years: Int(loanTermField.text!)!, interestRate: Double(interestRateField.text!)!).money
+        
+    }
     
+    //shift + command + 0 调出官方的参考文档
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // 文本框输入的时候，会成为  firstResponder
+        loanAmountField.resignFirstResponder()
+        loanTermField.resignFirstResponder()
+        interestRateField.resignFirstResponder()
+    }
+    
+    //guard 测试,有值为nil时才执行else，否则呢，继续往下执行， else里必须加 return
+    func demo(name:String?,age:Int?){
+        
+        guard let name = name,let age = age else {
+            print("name or age is nil")
+            return
+        }
+        print("没有空值")
+        print(name + String(age))
+    }
+
     //MARK:视图加载完成
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+//        demo(name: nil, age: 100)
+//        
+//        demo(name: "tom", age: 18)
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
     //MARK:接到内存警告
@@ -52,4 +103,27 @@ class simpleCalculate {
     }
 }
 
+// 复利的计算方法
+class compoundCalculate{
+    func calculate(loanAmount:Double,years:Int,interestRate:Double) -> Double {
+        let rate = interestRate / 100
+        let compoundInterest = pow(1.0+rate, Double(years))
+        return compoundInterest * loanAmount
+    }
+}
+// extension 扩展，可以扩展功能，扩展对象包括class ，enum，struct
+extension Double{
+    var money:String{
+        // 3.0 之前很多带NS前缀的函数，在3.0中都去掉了
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        // 定义一个可选类型 ？，表明它可以为nil
+        let result:String?
+        result = formatter.string(from: NSNumber(value:self))
+        if result == nil{
+            return "format error "
+        }
+        return result!
+    }
+}
 
